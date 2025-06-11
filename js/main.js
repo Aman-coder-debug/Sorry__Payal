826622939e0e08acbd6f4cb40416283c57d0fcd8
Aman-coder-debug <neev7180@gmail.com>
2025-06-11 13:52:45 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('SORRY SHALU ').split('')
+    const titles = ('SORRY PAYAL ').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
